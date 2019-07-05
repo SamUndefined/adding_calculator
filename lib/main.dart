58836028710c9b03f8257b91bd90b18fca3450f4
SamUndefined <sam.undefined.dev@gmail.com>
2019-07-05 14:11:29 -0500

    Add 0-9 buttons

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,76 @@ class HomePage extends StatelessWidget {
             message: 'Output',
             child: Text('0'),
           ),
+          Tooltip(
+            message: 'Zero',
+            child: FlatButton(
+              child: Text('0'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'One',
+            child: FlatButton(
+              child: Text('1'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'Two',
+            child: FlatButton(
+              child: Text('2'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'Three',
+            child: FlatButton(
+              child: Text('3'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'Four',
+            child: FlatButton(
+              child: Text('4'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'Five',
+            child: FlatButton(
+              child: Text('5'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'Six',
+            child: FlatButton(
+              child: Text('6'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'Seven',
+            child: FlatButton(
+              child: Text('7'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'Eight',
+            child: FlatButton(
+              child: Text('8'),
+              onPressed: () => {},
+            ),
+          ),
+          Tooltip(
+            message: 'Nine',
+            child: FlatButton(
+              child: Text('9'),
+              onPressed: () => {},
+            ),
+          ),
         ],
       ),
     );
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -12,4 +12,29 @@ void main() {
 
     expect(outputText, findsOneWidget);
   });
+
+  testWidgets('Shows buttons for 0-9', (WidgetTester tester) async {
+    await tester.pumpWidget(App());
+    final zero = find.byTooltip('Zero');
+    final one = find.byTooltip('One');
+    final two = find.byTooltip('Two');
+    final three = find.byTooltip('Three');
+    final four = find.byTooltip('Four');
+    final five = find.byTooltip('Five');
+    final six = find.byTooltip('Six');
+    final seven = find.byTooltip('Seven');
+    final eight = find.byTooltip('Eight');
+    final nine = find.byTooltip('Nine');
+
+    expect(zero, findsOneWidget);
+    expect(one, findsOneWidget);
+    expect(two, findsOneWidget);
+    expect(three, findsOneWidget);
+    expect(four, findsOneWidget);
+    expect(five, findsOneWidget);
+    expect(six, findsOneWidget);
+    expect(seven, findsOneWidget);
+    expect(eight, findsOneWidget);
+    expect(nine, findsOneWidget);
+  });
 }
